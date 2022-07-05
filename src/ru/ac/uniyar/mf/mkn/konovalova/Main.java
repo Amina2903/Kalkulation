e08dcbefc83fa698fc041065a0aad3a8ccdc1ee6
Амина <akonovalova2903@gmail.com>
2022-07-05 12:28:41 +0300

    целые числа-калькулятор

---
+++ src/ru/ac/uniyar/mf/mkn/konovalova/Main.java
@@ -0,0 +1,40 @@
+package ru.ac.uniyar.mf.mkn.konovalova;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int num1 = in.nextInt();
+        String str = in.next();
+        int num3 = in.nextInt();
+        System.out.println(num1 + " " + str + " " + num3);
+        int result = 0;
+        switch (str) {
+            case "+":
+                result = num1 + num3;
+                System.out.println(result);
+                break;
+            case "-":
+                result = num1 - num3;
+                System.out.println(result);
+                break;
+            case "*":
+                result = num1 * num3;
+                System.out.println(result);
+                break;
+            case "/":
+                if(num3 == 0)
+                    System.out.println("Деление на 0 невозможно");
+                else {
+                    result = num1 / num3;
+                    System.out.println(result);
+                }
+                break;
+            default:
+                System.out.println("Ошибка, некоректный ввод");
+        }
+        }
+    }
+
